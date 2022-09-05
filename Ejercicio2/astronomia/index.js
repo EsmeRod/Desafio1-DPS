df0136f3134d803d4e5c60e5b3caf95a81f4da1a
EsmeRod <89407510+EsmeRod@users.noreply.github.com>
2022-09-04 20:43:16 -0600

    imgs

--- Ejercicio2/astronomia/index.js
+++ Ejercicio2/astronomia/index.js
@@ -2,8 +2,76 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+ import React, {Component} from 'react';
+ import {AppRegistry} from 'react-native'
+ import {Text,View} from 'react-native';
+ import React from "react";
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,Image} from 'react-native';
+const DATA = [
+ {
+ id: '1',
+ title: 'Mercurio',
+ },
+ {
+ id: '2',
+ title: 'Venus',
+ },
+ {
+ id: '3',
+ title: 'La Tierra',
+ },
+ {
+    id: '4',
+    title: 'Estacion Espacial',
+},
+{
+    id: '5',
+    title: 'Luna',
+},
+{
+    id: '6',
+    title: 'Marte',
+},
+{
+    id: '7',
+    title: 'Ceres',
+}
+ ];
+ const Item = ({ title }) => (
+ <View style={styles.item}>
+ <Text style={styles.title}>{title}</Text>
+ </View>
+ );
 
-AppRegistry.registerComponent(appName, () => App);
+ const App = () => {
+ const renderItem = ({ item }) => (
+ <Item title={item.title} />
+ );
+
+ return (
+ <SafeAreaView style={styles.container}>
+ <FlatList
+ data={DATA}
+ renderItem={renderItem}
+ keyExtractor={item => item.id}
+ />
+ </SafeAreaView>
+ );
+ }
+
+ const styles = StyleSheet.create({
+ container: {
+ flex: 1,
+ marginTop: StatusBar.currentHeight || 0,
+ },
+ item: {
+ backgroundColor: '#f9c2ff',
+ padding: 20,
+ marginVertical: 8,
+ marginHorizontal: 16,
+ },
+ title: {
+ fontSize: 32,
+ },
+ });
+AppRegistry.registerComponent("cars", () => App);
